f27c3e1204449286a7d3cd16644bde1ba7267d60
rbenites <rbenites115@gmail.com>
2018-03-06 18:09:30 -0800

    added some java variables

---
+++ assets/Javascript/game.js
@@ -0,0 +1,15 @@
+//This will hold an list of words to guess in the game//
+var words=["obi wan", "yoda", "darth vader","darth sidius","JarJar","QuiGon","Anikin", "amadala"];
+
+//Allert if player wins//
+var winAlert=["You are winner. HAHAHA!"];
+
+//Alert if player wins//
+var loserAlert=["You've lost. FINSIH HIM!"];
+
+//other variables//
+var dashes=[];
+var guessed=[];
+var wins=0;
+var losses=0;
+var guessesRemaining=10;
